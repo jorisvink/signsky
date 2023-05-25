d053c0e40110d06f9e78a9867b4e3bfd6b5c7c29
Joris Vink <joris@coders.se>
2023-05-25 20:16:36 +0200

    typo

--- src/clear.c
+++ src/clear.c
@@ -113,7 +113,7 @@ clear_send_packet(int fd, struct signsky_packet *pkt)
 		break;
 	}
 
-	printf("clear -tx %p (%zd/%zu)\n", (void *)pkt, ret, pkt->length);
+	printf("clear-tx %p (%zd/%zu)\n", (void *)pkt, ret, pkt->length);
 
 	signsky_packet_release(pkt);
 }
